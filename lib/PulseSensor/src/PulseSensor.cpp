e0449fbbfd004c7849ee005746ab199b6aef34ef
Eliza Weisman <hi@hawkweisman.me>
2016-09-21 21:36:34 -0400

    add documentation

--- lib/PulseSensor/src/PulseSensor.cpp
+++ lib/PulseSensor/src/PulseSensor.cpp
@@ -8,6 +8,7 @@ void _isr_wrapper() {
 }
 
 void PulseSensor::init_interrupts() {
+    // The Part Where The Arduino Timer Libraries Each Have A Completely Different Idiom
     #if (PSENSOR_TIMER1)
         Timer1.initialize(_2MS_MICROSECONDS);
         Timer1.attachInterrupt(_isr_wrapper);
@@ -16,6 +17,7 @@ void PulseSensor::init_interrupts() {
         MsTimer2::set(_2MS_MILLISECONDS, _isr_wrapper);
         MsTimer2::start();
     #else
+        // this probably doesn't happen but You Never Know!
         #error Must define a timer for the pulse sensor!
     #endif
 }
